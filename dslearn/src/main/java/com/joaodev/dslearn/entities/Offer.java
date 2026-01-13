1995edf16cf68c5309a081154d6219cc09eb3e42
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2026-01-13 12:24:09 -0300

    Resource

--- dslearn/src/main/java/com/joaodev/dslearn/entities/Offer.java
+++ dslearn/src/main/java/com/joaodev/dslearn/entities/Offer.java
@@ -1,13 +1,17 @@
 package com.joaodev.dslearn.entities;
 
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -18,12 +22,19 @@ public class Offer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String edition;
+    
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant startMoment;
+    
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant endMoment; 
 
     @ManyToOne
     @JoinColumn(name = "course_id")
     private Course course;
+
+    @OneToMany(mappedBy = "offer")
+    List<Resource> resources = new ArrayList<>();
 
     public Offer(){
     }
@@ -74,6 +85,10 @@ public class Offer {
 
     public void setCourse(Course course) {
         this.course = course;
+    }
+
+     public List<Resource> getResources() {
+        return resources;
     }
 
     @Override
